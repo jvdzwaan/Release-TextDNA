65088ba910a2bda930bd2be954accc67e6a8ede8
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-03-28 17:26:59 +0200

    Rename name-prefix to name_prefix
    Because this is more consistent with the names of the other parameters.

--- textdnaDataGen/generate.py
+++ textdnaDataGen/generate.py
@@ -10,7 +10,7 @@ parser.add_argument('corpus_path', help='path to corpus to tag relative to the l
 parser.add_argument('--output_dir', help='path to output folder relative to the location of this script', default=os.curdir)
 parser.add_argument('mode', help='flag for indicating whether the dataset is n-gram or raw text (options: ngram OR word_sequence')
 parser.add_argument('--folder_sequences', help='treats named subfolders rather than individual text files as sequences. for use with ngram mode ONLY.', action="store_true")
-parser.add_argument('--name-prefix', '-n', help='name prefix of the output file')
+parser.add_argument('--name_prefix', '-n', help='name prefix of the output file')
 
 
 if __name__ == '__main__':
